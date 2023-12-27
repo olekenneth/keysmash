f4f63c1f015eb157a40a647500d8a41187277216
Ole-Kenneth Bratholt <ok@schibsted.com>
2023-12-27 18:24:39 +0100

    First version

---
+++ game.js
@@ -0,0 +1,166 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+let audioCtx = null;
+let oscillator = null;
+let gain = null;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const fontSize = 20;
+
+const levels = [
+  { speed: 10, letters: ["A", "S", "D", "F", "G"] },
+  { speed: 15, letters: ["H", "J", "K", "L"] },
+  {
+    speed: 20,
+    letters: ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  },
+  {
+    speed: 25,
+    letters: ["Q", "W", "E", "R", "T", "A", "S", "D", "F", "G"],
+  },
+  {
+    speed: 30,
+    letters: ["Y", "U", "I", "O", "P", "H", "J", "K", "L"],
+  },
+];
+
+let level = levels[0];
+let score = 0;
+
+const getRandomLetter = () =>
+  level.letters[Math.floor(Math.random() * level.letters.length)];
+
+let board = [];
+let activeCols = [];
+
+const resetBoard = (nextLevel) => {
+  board = Array(Math.floor(canvas.height / fontSize))
+    .fill("")
+    .map(() => Array(Math.floor(canvas.width / fontSize)).fill(""));
+
+  for (const col in board[0]) {
+    board[0][col] = getRandomLetter();
+  }
+
+  if (nextLevel) {
+    activeCols = [];
+    console.log("next level", level);
+    const index = levels.findIndex((l) => l.speed === level.speed);
+    if (levels[index + 1]) {
+      level = levels[index + 1];
+    } else {
+      level.speed += 10;
+    }
+  }
+};
+
+resetBoard();
+
+let offset = fontSize;
+
+const setupCanvas = (ctx) => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // background
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "green";
+  ctx.font = `${fontSize}px Arial`;
+  ctx.textAlign = "right";
+  ctx.textBaseline = "bottom";
+};
+
+const drawBoard = () => {
+  for (const row in board) {
+    for (const col in board[row]) {
+      const letter = board[row][col];
+      if (letter) {
+        ctx.fillText(letter, col * fontSize, row * fontSize);
+      }
+    }
+  }
+};
+
+const getRandomIndex = () => Math.floor(Math.random() * board[0].length);
+
+const addActiveCol = () => {
+  const randomIndex = getRandomIndex();
+  activeCols.push({ letter: board[0][randomIndex], x: randomIndex, y: 0 });
+};
+
+setInterval(() => {
+  setupCanvas(ctx);
+
+  activeCols.forEach((col) => {
+    const row = col.y + 1;
+    if (board[row]) {
+      board[row][col.x] = col.letter;
+      col.y = row;
+    }
+  });
+
+  if (Math.random() < 0.1 || activeCols.length === 0) {
+    addActiveCol();
+  }
+
+  drawBoard();
+}, 800 - level.speed * 50);
+
+document.addEventListener("keydown", (event) => {
+  event.preventDefault();
+  const { key } = event;
+
+  if (!activeCols[0]) return console.log("no active cols");
+
+  if (activeCols[0].letter === key.toUpperCase()) {
+    const activeCol = activeCols.shift();
+    for (const row in board) {
+      if (row == 0) continue;
+      board[row][activeCol.x] = "";
+    }
+
+    //playFrequency(440);
+    console.log("yey", score, level);
+    score++;
+    playFrequency(440, "sine");
+    if (score === level.speed) {
+      resetBoard(true);
+    }
+  } else {
+    playFrequency(87.31, "triangle");
+
+    console.log(board);
+    console.log(activeCols);
+    console.log("wrong key", activeCols[0].letter, key.toUpperCase());
+  }
+});
+
+const playFrequency = async (frequency, type) => {
+  if (!audioCtx) audioCtx = new AudioContext();
+  if (!type) type = "sine";
+  oscillator = audioCtx.createOscillator();
+  gain = audioCtx.createGain();
+  oscillator.type = type;
+  oscillator.connect(gain);
+  oscillator.frequency.value = frequency;
+  gain.connect(audioCtx.destination);
+  oscillator.start(0);
+
+  gain.gain.exponentialRampToValueAtTime(0.00001, audioCtx.currentTime + 1);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 140);
+  });
+};
+
+const playPopcorn = async () => {
+  const notes = [65.41, 58.27, 65.41, 49.0, 38.89, 49.0, 32.7];
+
+  for (const freq of notes) {
+    await playFrequency(freq, "sine");
+  }
+  playPopcorn();
+};
